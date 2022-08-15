6ccc1c9fe64ddfdaa8fbb2928d219746a236159c
Alexey <alekseipodlyagin@mail.ru>
2022-08-15 20:53:26 +0000

    Correct model_testing.py

--- model_testing.py
+++ model_testing.py
@@ -5,9 +5,9 @@ from model_preparation import model, X_test
 #Предсказание на тестовых данных
 y_test=model.predict(X_test)
 
-#Записть в столбец 'polution'
+#Запись в столбец 'polution'
 Submission = pd.DataFrame(y_test, columns = ["polution"])
 
 #Перезапись в файл
 Submission.to_csv('My_Submission.csv',index=False)
-print(Submission.head())
+print(My_Submission.head())
